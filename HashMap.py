65c1ce6ccc526336f725319f6678b4957993ab2c
4ist <fpailes@DESKTOP-T0Q4PSP.localdomain>
2022-05-16 14:56:04 -0600

    added retrieve and assign methods

--- HashMap.py
+++ HashMap.py
@@ -3,7 +3,7 @@ class HashMap:
 
     def __init__(self, array_size):
         self.array_size = array_size
-        self.array = [ None for i in range(array_size)
+        self.array = [ [] for i in range(array_size)]
 
     def hash(self, key):
         my_hash = key.encode()
@@ -12,5 +12,30 @@ class HashMap:
     def compress(self, my_hash):
         return my_hash % self.array_size
 
+# Assigns by appending the key value pair to the end of the list, be it empty or not.
     def assign(self, key, value):
-        
+        array_index = self.compress(self.hash(key))
+
+        if self.array[array_index] == []:
+            self.array[array_index].append([key, value])
+            print(self.array[array_index])
+            return
+        for tuplepair in self.array[array_index]:
+            if tuplepair[0] == key:
+                tuplepair[1] = value
+                return
+
+    def retrieve(self, key):
+        array_index = self.compress(self.hash(key))
+
+        if len(self.array[array_index]) >= 1:
+            for keyval in self.array[array_index]:
+                if keyval[0] == key:
+                    return keyval[1]
+
+
+new_hash = HashMap(10)
+
+new_hash.assign('penis', 'balls')
+
+print(new_hash.retrieve('penis'))
