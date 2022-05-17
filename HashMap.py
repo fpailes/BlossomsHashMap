bed577fe23a66b24de8cce4eaa31b8d09b6f0776
4ist <fpailes@DESKTOP-T0Q4PSP.localdomain>
2022-05-17 09:59:18 -0600

    fixed bugs in assign method, added testing

--- HashMap.py
+++ HashMap.py
@@ -5,6 +5,7 @@ class HashMap:
         self.array_size = array_size
         self.array = [ [] for i in range(array_size)]
 
+
     def hash(self, key):
         my_hash = key.encode()
         return sum(my_hash)
@@ -12,23 +13,38 @@ class HashMap:
     def compress(self, my_hash):
         return my_hash % self.array_size
 
-# Assigns by appending the key value pair to the end of the list, be it empty or not.
+# Assigns by appending the key value pair to the end of the list, unless it has the same key, in which case overwrites the value..
     def assign(self, key, value):
+        if_all = True
         array_index = self.compress(self.hash(key))
 
         if self.array[array_index] == []:
             self.array[array_index].append([key, value])
-            print(self.array[array_index])
-            return
-        for tuplepair in self.array[array_index]:
-            if tuplepair[0] == key:
-                tuplepair[1] = value
-                return
+            if_all = False
+        for tuples in self.array[array_index]:
+            if tuples[0] == key:
+                tuples[1] = value
+                if_all = False
+        if if_all:
+            self.array[array_index].append([key, value])
 
+# Retrieves values by looping through the list at index self.array[array_index]
     def retrieve(self, key):
         array_index = self.compress(self.hash(key))
 
+        for i in self.array:
+            print(i)
         if len(self.array[array_index]) >= 1:
             for keyval in self.array[array_index]:
                 if keyval[0] == key:
                     return keyval[1]
+
+new_hash = HashMap(7)
+
+new_hash.assign('test', 'case')
+new_hash.assign('testing', 'casing')
+new_hash.assign('testing', 'purple')
+new_hash.assign('pink', 'tester')
+new_hash.assign('butts', 'random')
+print(new_hash.retrieve('testing'))
+#print(new_hash.retrieve('test'))
